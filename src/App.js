2be53c1057aded5734292b63a36e28d98ae66edf
toorki <torkilaroussi@gmail.com>
2023-01-22 14:54:54 +0100

    cloud integration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MAP INTEGRATION</h1>
+      <p>i'm using this map application to locate my position instantantly, while working on this project!!!!</p>
+      <iframe width="100%" height="600" id="gmap_canvas" src="https://maps.google.com/maps?q=cite%20hedi%20nouira&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}
+
+export default App;
